c6526a3010a84e7c24a28338fb25ed18ce98457f
Stefo16 <76417700+Stefo16@users.noreply.github.com>
2021-08-24 01:55:01 -0400

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,12 @@
+color = input("Enter a color: ")
+country = input("Enter a country: ")
+city = input("Enter a capital city: ")
+animal = input("Enter your favourite animal: ")
+celebrity = input("Enter a celebrity: ")
+
+
+print("Roses are " + color)
+print("Your "+ country+" is blue")
+print("Once lived a person by the name of "+ celebrity +" in "+ city +" they were raised.")
+print("A true hero they really were until the Presidents "+ animal +" ate them. Then everyone was phased.")
+
